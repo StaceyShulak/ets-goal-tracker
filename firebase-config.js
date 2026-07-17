71f93ccc7ed9f6d2b6fce2b95f0383d7b95b2186
STACEY SHULAK, BA., CRHA Consultante en gestion de talent – Talent Management Consultant <47359128+StaceyShulak@users.noreply.github.com>
2026-07-17 14:19:28 -0400

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,12 @@
+// Firebase Configuration for ETS Goal Tracker
+// DO NOT COMMIT TO GITHUB - Keep this file private
+
+window.firebaseConfig = {
+  apiKey: "AIzaSyB4fR4dZiZUcgzmP7hlhEiYVT-vUbgoeXc",
+  authDomain: "ets-goal-tracker.firebaseapp.com",
+  databaseURL: "https://ets-goal-tracker-default-rtdb.firebaseio.com",
+  projectId: "ets-goal-tracker",
+  storageBucket: "ets-goal-tracker.firebasestorage.app",
+  messagingSenderId: "589590410096",
+  appId: "1:589590410096:web:6ae8b16ffbcd6af6c555b5"
+};
